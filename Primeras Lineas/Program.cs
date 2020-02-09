c831c5024b1e1aca13c002d41ed1261b33b360b8
Brayan Estrada <brayaneq7@gmail.com>
2020-02-09 22:27:17 +0100

    prueba 1

--- Primeras Lineas/Program.cs
+++ Primeras Lineas/Program.cs
@@ -2,11 +2,34 @@
 
 namespace Primeras_Lineas
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            //Conseguir las variables accecdiendo desde otra cs
+            Variables p1 = new Variables();
+
+            Console.WriteLine("Introduce el ancho de la habitacion:");
+            p1.dato1 = Console.ReadLine();
+
+            Console.WriteLine("Introduce el largo de la habitacion:");
+            p1.dato2 = Console.ReadLine();
+            
+            
+            //Permite los decimales
+            p1.ancho = Convert.ToDouble(p1.dato1);
+            p1.largo = Convert.ToDouble(p1.dato2);
+
+            while (p1.dato1 != null && p1.dato2 != null)
+            {
+
+                p1.DameArea();
+                Console.WriteLine(p1.area);
+            }
+
+            Console.ReadKey();
+
         }
     }
 }
---
+++ Primeras Lineas/Variables.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Primeras_Lineas
+{
+    public class Variables
+    {
+
+        public double area ;
+        public double ancho ;
+        public double largo ;
+        public string dato1 ;
+        public string dato2 ;
+
+        public void DameArea()
+        {
+            area = largo * ancho;
+
+        }
+
+    }
+}
